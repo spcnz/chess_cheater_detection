6c15446c5880706c8be3f01af0ee48a5ef947cb1
Milena Laketic <laketic.milena98@gmail.com>
2023-09-10 19:57:42 +0200

    Remove unused file

--- infrastructure/produce.py
+++
@@ -1,5 +0,0 @@
-from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-for _ in range(100):
-   producer.send('yauza.test', b"lalalalal")
-   producer.flush()
