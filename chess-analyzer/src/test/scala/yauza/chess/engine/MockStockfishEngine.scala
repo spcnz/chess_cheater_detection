4b0bb2ec7e49c3316aaa70b674e9765a3e21f365
Milena Laketic <laketic.milena98@gmail.com>
2023-10-25 03:14:00 +0200

    Add tests

--- chess-analyzer/src/test/scala/yauza/chess/engine/MockStockfishEngine.scala
+++ chess-analyzer/src/test/scala/yauza/chess/engine/MockStockfishEngine.scala
@@ -1,11 +1,17 @@
 package yauza.chess.engine
 
+import yauza.chess.`enum`.{PlayerColor, ScoreType}
+
 class MockStockfishEngine extends Engine {
   override def start(): Unit = {}
 
+  override def getPointOfView(fen: String): String = PlayerColor.White
+
+  override def getPlayerScore(fen: String): (Long, String) =
+    if (fen.length > 30)
+      (fen.length, ScoreType.Centipawn)
+    else
+      (-1 * fen.length, ScoreType.Centipawn)
+
   override private[engine] def setPosition(fen: String): Unit = {}
-
-  override def getPointOfView(fen: String) = "???"
-
-  override def getPlayerScore(fen: String) = (100, "CENTIPAWN")
 }
